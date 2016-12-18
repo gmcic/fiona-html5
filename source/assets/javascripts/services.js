d3fc2603821a875a53981a0736a2e6a148302b33
tom <tom@daoshenggroup.com>
2016-12-18 10:46:00 +0800

    modify ip

--- source/assets/javascripts/services.js
+++ source/assets/javascripts/services.js
@@ -37,13 +37,13 @@ angular.module('fiona.services', [])
         getBusinessHostname : function () {
 //            return "http://192.168.0.254:8080/business";
 
-            return "http://192.168.0.108:8080/business";
+            return "http://localhost:8080/business";
         },
         getAccountHostname : function () {
 
 //            return "http://192.168.0.254:8080/account";
 
-            return "http://192.168.0.108:8080/account";
+            return "http://localhost:8080/account";
         },
         modaldanger: function (alert_id, msg) {
             App.alert({
@@ -166,7 +166,7 @@ angular.module('fiona.services', [])
             var _db = this.getLocalDB();
 
 //            var baseURL = "http://192.168.0.254:8080/business/api/v2/";
-            var baseURL = "http://192.168.0.108:8080/business/api/v2/";
+            var baseURL = "http://localhost:8080/business/api/v2/";
 
             // 重新创建表
             angular.forEach(["dicttypedetails", "userdictdetails", 'itemcates', 'itemtypes'], function(name){
