87404c3215233568a0850461a3451e02317f78ec
tim <tim@163.com>
2017-06-19 09:27:43 +0800

    菜单|角色

---
+++ source/assets/pages/basic/MenusController.js
@@ -0,0 +1,176 @@
+// 角色管理
+angular.module('fiona').controller('MenusController', function ($scope, $controller, $http, commons) {
+
+  $scope.dropdowns = {};
+
+  commons.findDict($scope.dropdowns, {petRaceNameSet: "厂家类型"});
+
+  $controller('BaseController', {$scope: $scope}); //继承
+
+  /**
+   * 菜单管理
+   * ---------------------------
+   * */
+  $scope.menusportal = {
+
+    id: "menus",
+
+    name: "角色管理",
+
+    defilters: {},
+
+    server: "/api/v2/menus",
+
+    callback: {
+      insert: function () {
+        $scope.parentName = $scope.menusideportal.selected.name;
+        $scope.menus = {parentId: $scope.menusideportal.selected.id, parentCode: $scope.menusideportal.selected.code, iconClass: 'nav-item'};
+      }
+    }
+  };
+
+  $controller('BaseCRUDController', {$scope: $scope, component: $scope.menusportal}); //继承
+
+  /**
+   * 搜索不分页
+   * ---------------------------
+   * */
+  $scope.menusportal.query = function (parentCode) {
+    $http.post(commons.getAccountHostname() + this.server + "/page" + commons.getTimestampStr(), {
+      pageSize: 1,
+      pageNumber: 100000,
+      filters: [{
+        "fieldName": "parentCode",
+        "operator": "LIKE",
+        "value": parentCode
+      }]
+    }).success(function (data, status, headers, config) {
+
+      // this.selectionReset();
+
+      console.log("菜单列表", data.data);
+
+      $scope['menuss'] = data.data;
+    });
+  };
+
+
+  /**
+   * 保存
+   * ---------------------------
+   * */
+  $scope.menusportal.submit = function () {
+
+    console.log("submit", $scope.menus);
+    $scope[this.id + "form"].submitted = true;
+
+    if ($scope[this.id + "form"].$valid) {
+
+      $http.post(commons.getAccountHostname() + this.server + commons.getTimestampStr(), $scope[this.id]).success(function (data, status, headers, config) {
+
+        $scope[$scope.menusportal.id] = data.data;
+
+        // $scope.menusportal.refresh();
+
+        $scope['menusides'] = data.data;
+
+        $scope.menusideportal.list();
+        $scope.menusideportal.treeConfig.version++;
+
+        $('#' + $scope.menusportal.id).modal('hide');
+
+        commons.success("保存成功")
+      }).error(function (data, status, headers, config) { //     错误
+
+        commons.modaldanger($scope.menusportal.id, "保存失败")
+      });
+    }
+  };
+
+
+  /**
+   * 菜单
+   * ---------------------------
+   * */
+  $scope.menusideportal = {
+
+    id: "menuside",
+
+    name: "菜单",
+
+    defilters: {},
+
+    server: "/api/v2/menus",
+
+    /**
+     * 目录树配置
+     * ---------------------------
+     * */
+    treeConfig: {
+      core: {
+        themes: {responsive: !1},
+        multiple: true,
+        animation: false,
+        error: function (error) {
+          alert("error from js tree - " + angular.toJson(error));
+        },
+        check_callback: true,
+        worker: true
+      },
+      types: {
+        default: {
+          icon: 'fa fa-folder icon-state-warning icon-lg'
+        },
+        file: {icon: "fa fa-file icon-state-warning icon-lg"}
+      },
+      version: 1,
+      plugins: ['types']
+    },
+
+    // 树事件
+    treeEventsObj: {
+      'select_node': function (e, item) {
+
+        $scope.menusideportal.selectedId = item.selected[0];
+
+        angular.forEach($scope.menusides, function (data) {
+
+          // console.log('EQ', {"SELECT ID": $scope.menusideportal.selectedId, "DATA ID": data.id});
+
+          if (data.id == $scope.menusideportal.selectedId) {
+            $scope.menusideportal.selected = data;
+            $scope.menuside = data;
+          }
+        });
+
+        // 查询子节点
+        $scope.menusportal.query($scope.menusideportal.selected.code);
+
+        // console.log('selectedId', $scope.menusideportal.selectedId);
+        // console.log('select_node', $scope.menusideportal.selected);
+      }
+    },
+
+    list: function () {
+      $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {}).success(function (data, status, headers, config) {
+
+        // console.log("菜单", data.data);
+
+        angular.forEach(data.data, function (item) {
+          item.text = item.name;
+          item.parent = item.parentId || "#";
+          // item.parent = "#";
+          // // console.log("Text: " + item.text)
+          // // console.log("Parent: " + item.parent)
+        });
+
+        $scope['menusides'] = data.data;
+
+        $scope.menusideportal.treeConfig.version++;
+      });
+    }
+  };
+
+  // $scope.menusportal.list();
+  $scope.menusideportal.list();
+});
--- source/assets/pages/basic/RoleController.js
+++ source/assets/pages/basic/RoleController.js
@@ -1,35 +1,221 @@
-
 // 角色管理
-angular.module('fiona').controller('RoleController', function($scope, $controller, $http, commons) {
+angular.module('fiona').controller('RoleController', function ($scope, $controller, $http, commons) {
 
-    $scope.dropdowns = {};
+  $scope.dropdowns = {};
 
-    commons.findDict($scope.dropdowns, {petRaceNameSet: "厂家类型"});
+  commons.findDict($scope.dropdowns, {petRaceNameSet: "厂家类型"});
 
-    $controller('BaseController', {$scope: $scope}); //继承
+  $controller('BaseController', {$scope: $scope}); //继承
+
+  /**
+   * 角色
+   * ---------------------------
+   * */
+  $scope.roleportal = {
+
+    id: "role",
+
+    name: "角色管理",
+
+    defilters: {},
+
+    server: "/api/v2/roles",
+
+    callback: {
+      insert: function () {
+        $scope.setSelectDefault("role", ["companyType"]);
+      },
+      update: function () {
+        // 查义用户和树
+        var _role = $scope.role;
+
+        $scope.rolemenuportal.query(_role.code);
+        $scope.userroleportal.query(_role.id);
+      }
+    }
+  };
+
+  $controller('BaseCRUDController', {$scope: $scope, component: $scope.roleportal}); //继承
+
+  /**
+   * 搜索不分页
+   * ---------------------------
+   * */
+  $scope.roleportal.list = function () {
+    $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {}).success(function (data, status, headers, config) {
+
+      // this.selectionReset();
+
+      console.log("角色列表", this.id, data.data);
+
+      $scope['roles'] = data.data;
+    });
+  };
+
+
+  /**
+   * 用户
+   * ---------------------------
+   * */
+  $scope.userportal = {
+
+    id: "user",
+
+    name: "用户",
+
+    defilters: {},
+
+    server: "/api/v2/users"
+  };
+
+  $controller('BaseCRUDController', {$scope: $scope, component: $scope.userportal}); //继承
+
+  $scope.userportal.list = function () {
+    $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {}).success(function (data, status, headers, config) {
+
+      // this.selectionReset();
+
+      console.log("用户列表", this.id, data.data);
+
+      $scope['users'] = data.data;
+    });
+  };
+
+  /**
+   * 角色-用户
+   * ---------------------------
+   * */
+  $scope.userroleportal = {
+
+    id: "userrole",
+
+    name: "角色-用户",
+
+    defilters: {},
+
+    server: "/api/v2/userroles"
+  };
+
+  $controller('BaseCRUDController', {$scope: $scope, component: $scope.userroleportal}); //继承
+
+  $scope.userroleportal.query = function (roleId) {
+    $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {filters: [{"fieldName": "roleId", "operator": "EQ", "value":roleId}]}).success(function (data, status, headers, config) {
+
+      // this.selectionReset();
+
+      console.log("角色-用户", this.id, data.data);
+
+      $scope['userroles'] = data.data;
+    });
+  };
+
+  /**
+   * 角色-菜单
+   * ---------------------------
+   * */
+  $scope.rolemenuportal = {
+
+    id: "rolemenu",
+
+    name: "角色-菜单",
+
+    defilters: {},
+
+    server: "/api/v2/rolemenus"
+  };
+
+  $controller('BaseCRUDController', {$scope: $scope, component: $scope.rolemenuportal}); //继承
+
+  $scope.rolemenuportal.query = function (roleCode) {
+    $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {filters: [{"fieldName": "roleCode", "operator": "EQ", "value":roleCode}]}).success(function (data, status, headers, config) {
+
+      // this.selectionReset();
+
+      console.log("角色-菜单", this.id, data.data);
+
+      $scope['rolemenu'] = data.data;
+    });
+  };
+
+
+  /**
+   * 菜单
+   * ---------------------------
+   * */
+  $scope.menusportal = {
+
+    id: "menus",
+
+    name: "菜单",
+
+    defilters: {},
+
+    server: "/api/v2/menus",
+
 
     /**
-     * 经销商与生产商
+     * 目录树配置
      * ---------------------------
      * */
-    $scope.roleportal= {
+    treeConfig: {
+      core: {
+        themes: {responsive: !1},
+        multiple: true,
+        animation: false,
+        error: function (error) {
+          alert("error from js tree - " + angular.toJson(error));
+        },
+        check_callback: true,
+        worker: true
+      },
+      checkbox: {
 
-        id: "role",
+        tie_selection:false,
+      },
+      types: {
+        default: {
+          icon: 'fa fa-folder icon-state-warning icon-lg'
+        },
+        file: {icon: "fa fa-file icon-state-warning icon-lg"}
+      },
+      version: 1,
+      plugins: ['types', 'checkbox']
+    },
 
-        name: "角色管理",
+    // 树事件
+    treeEventsObj: {
+      'check_node': function (e, item) {
+        console.log("scope", $scope);
+        console.log('check_node', item);
+      },
 
-        defilters: {},
+      'uncheck_node': function (e, item) {
+        console.log("uncheck_node", item);
+      }
+    },
 
-        server: "/api/v2/roles",
+    list: function () {
+      $http.post(commons.getAccountHostname() + this.server + "/list" + commons.getTimestampStr(), {}).success(function (data, status, headers, config) {
 
-        callback: {
-            insert: function () {
-                $scope.setSelectDefault("role", ["companyType"]);
-            }
-        }
-    };
+        console.log("菜单", data.data);
 
-    $controller('BaseCRUDController', {$scope: $scope, component: $scope.roleportal}); //继承
+        angular.forEach(data.data, function (item) {
+          item.text = item.name;
+          item.parent = item.parentId || "#";
+          // item.parent = "#";
+          // // console.log("Text: " + item.text)
+          // // console.log("Parent: " + item.parent)
+        });
 
-    $scope.roleportal.list();
+        $scope['menuss'] = data.data;
+
+        $scope.menusportal.treeConfig.version++;
+      });
+    }
+  };
+
+  // 初始化
+  $scope.roleportal.list();
+  $scope.userportal.list();
+  $scope.menusportal.list();
 });
