f98ac21b8d23b6795469e554554f456ebc6505aa
bqw_5189 <bqw_5189@163.com>
2019-06-20 17:06:35 +0800

    add item day

--- source/assets/pages/report/ItemController.js
+++ source/assets/pages/report/ItemController.js
@@ -61,14 +61,67 @@ angular.module('fiona').controller('ItemController', function ($scope, $http, co
           ]
       });
   }
+
+var showItemsMonthData = function(){
+      var table = $('#item_month');
+
+      table.dataTable({
+          "ordering": true,
+          searching:true,
+          autoWidth:false,
+          language: {
+              aria: {
+                  sortAscending: ": activate to sort column ascending",
+                  sortDescending: ": activate to sort column descending"
+              },
+              emptyTable: "无记录",
+              info: "显示 _START_ 到 _END_ 总 _TOTAL_ 记录",
+              infoEmpty: "没有找到记录",
+              infoFiltered: "(从 _MAX_ 条记录过滤)",
+              lengthMenu: "每页 _MENU_ 条记录",
+              search: "搜索:",
+              zeroRecords: "未找到匹配记录",
+              paginate: {
+                  previous: "上一页",
+                  next: "下一页",
+                  last: "尾页",
+                  first: "首页"
+              }
+          },destroy:true
+          ,data: $scope.reportItemMonthData,columns: [
+              { title: "名称",data: 'itemName',"width": "20%" },
+              { title: "销售数量",data: 'totalNum' },
+              { title: "销售均价",data: 'avgPrice' },
+              { title: "销售金额",data: 'total' ,orderable:true},
+              { title: "平均进价",data: 'avgInputPrice' },
+              { title: "总成本",data: 'totalCost' },
+              { title: "库存",data: 'inventory' },
+              { title: "时间",data: 'createDate' },
+          ],columnDefs:[
+              {
+                  targets:[2,3,4,5],
+                  render:function(data, type, full){
+                      return Number(data).toFixed(2);
+                  }
+              }
+
+          ],"order": [
+              [3, "desc"]
+          ]
+      });
+  }
+
   var initItemsData = function(company, year,month, day){
     $http.get(commons.getBusinessHostnameByCompany(company) + "/api/v2/reports/item/day?year="+year+"&month="+month+"&day=" + day).success(function (data, status, headers, config) {
-        if ($scope.reportItemData.length === 0){
-            $scope.reportItemData = data.data;
-        }else{
-            $scope.reportItemData = $scope.reportItemData.concat(data.data);
-        }
+        $scope.reportItemData = data.data;
         showItemsData();
+    });
+  }
+
+  var initItemsMonthData = function(company, year,month){
+    $http.get(commons.getBusinessHostnameByCompany(company) + "/api/v2/reports/item/month?year="+year+"&month="+month).success(function (data, status, headers, config) {
+        $scope.reportItemMonthData = data.data;
+        showItemsMonthData();
     });
   }
 
@@ -96,9 +149,10 @@ angular.module('fiona').controller('ItemController', function ($scope, $http, co
   $scope.selectYear = $scope.currentYear;
   $scope.currentMonth = new Date().getMonth() + 1;
   $scope.selectMonth = $scope.currentMonth;
-  $scope.compays = [{'label':'-','value':'-'}, {'label':'亦庄','value':'bj'}, {'label':'廊坊','value':'lf'}];
+  $scope.compays = [{'label':'亦庄','value':'bj'}, {'label':'廊坊','value':'lf'}];
   $scope.currentCompany = $scope.compays[0];
   $scope.reportItemData = [];
+  $scope.reportItemMonthData = [];
 
   $scope.maxDays=daysNumOfMonth();
 
@@ -133,13 +187,14 @@ angular.module('fiona').controller('ItemController', function ($scope, $http, co
       $scope.fosterCount=0;
 
       if ($scope.currentCompany.value === '-'){
-          $scope.compays.forEach(function (c) {
-              if (c.value != '-'){
-                  initItemsData(c, $scope.selectYear,$scope.selectMonth,$scope.currentDay)
-              }
-          });
+          // $scope.compays.forEach(function (c) {
+          //     if (c.value != '-'){
+          //         initItemsData(c, $scope.selectYear,$scope.selectMonth,$scope.currentDay)
+          //     }
+          // });
       }else{
           initItemsData($scope.currentCompany, $scope.selectYear,$scope.selectMonth,$scope.currentDay)
+          initItemsMonthData($scope.currentCompany, $scope.selectYear,$scope.selectMonth)
       }
   }
 
